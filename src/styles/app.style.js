b89aa46eb2b21a4774494b77a9fc2cd22f5d34c7
quanghuy1242 <katygaga124@gmail.com>
2019-09-17 16:07:21 +0700

    style delete button

--- src/styles/app.style.js
+++ src/styles/app.style.js
@@ -103,9 +103,13 @@ export const button = css`
     font-size: 1.5rem;
     font-weight: 700;
     line-height: 1;
-    color: #000;
+    color: black;
     text-shadow: 0 1px 0 #fff;
     opacity: .5;
+  }
+
+  .close:hover {
+    color: #dc3545;
   }
 
   button.close {
