6f3ad9781824b7a289c520e9fc9bf1f6851c4858
quanghuy1242 <katygaga124@gmail.com>
2019-09-20 01:47:30 +0700

    remove build, source map only at dev

--- webpack.config.js
+++ webpack.config.js
@@ -2,40 +2,25 @@ const merge = require('webpack-merge');
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-// // For IE Support
-// const { createCompatibilityConfig } = require('@open-wc/building-webpack');
-// const configs = createCompatibilityConfig({
-//   input: path.resolve(__dirname, './index.html'),
-// });
-
-// module.exports = [
-//   merge(configs[0], {
-//     plugins: [
-//       new CopyWebpackPlugin([
-//         'style.css',
-//       ]),
-//     ],
-//   }),
-//   configs[1],
-// ];
-
-// For modern browsers
 const { createDefaultConfig } = require('@open-wc/building-webpack');
 const config = createDefaultConfig({
   input: path.resolve(__dirname, './index.html'),
 });
 
-module.exports = merge(config, {
-  output: {
-    publicPath: '/'
-  },
-  plugins: [
-    new CopyWebpackPlugin([
-      'style.css',
-    ]),
-  ],
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true
-  }
-})
+module.exports = (env, argv) => {
+  return merge(config, {
+    output: {
+      publicPath: '/'
+    },
+    devtool: argv.mode !== 'production' ? 'source-map' : 'false',
+    plugins: [
+      new CopyWebpackPlugin([
+        'style.css',
+      ]),
+    ],
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      compress: true
+    }
+  })
+}
